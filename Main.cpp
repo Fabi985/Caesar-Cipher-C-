bacc4fd7c9e0fff62aa8ed6ddb27e2755a1d8f7d
Fabi Velea <115619516+Fabi985@users.noreply.github.com>
2024-09-17 22:40:36 +0100

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Encrypts from char to int
+    char a = 'z';
+    int ia = (int)a - 96; 
+
+    cout << ia;
+
+    // Decrypts from int to char
+    int b = 1 + 96;
+    char ib = (char)b;
+
+    cout << ib;
+
+    // The message i want to encrypt
+    string plaintext = "Hello world!";
+    // declare the array
+    
+    // For loop the plaintext and turn each char into an integer whilst appending each int to the array
+
+    // For loop again the array and add the key number to the stored nums
+}
